a79c97f64bc0aadb6d61d84b0e3ff37dcc229f2c
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 00:06:55 +0530

    Initial Commit

---
+++ SwiftUI-PathsAndShapes/SwiftUI_PathsAndShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PathsAndShapesApp.swift
+//  SwiftUI-PathsAndShapes
+//
+//  Created by Pradyumn Shukla on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PathsAndShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
