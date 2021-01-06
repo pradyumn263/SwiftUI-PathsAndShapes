e9f12d3b36510e8c6f49ab61f348445fa0322d21
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 12:12:38 +0530

    Stroke and Fill on the same path

--- SwiftUI-PathsAndShapes/ContentView.swift
+++ SwiftUI-PathsAndShapes/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path() { path in
-            path.move(to: CGPoint(x: 20, y: 60))
-            path.addLine(to: CGPoint(x: 40, y: 60))
-            path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 20))
-            path.addLine(to: CGPoint(x: 230, y: 60))
-            path.addLine(to: CGPoint(x: 230, y: 100))
-            path.addLine(to: CGPoint(x: 20, y: 100))
-            path.closeSubpath()
+        ZStack {
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 20))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.closeSubpath()
+            }
+            .stroke(Color.black, lineWidth: 5)
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 20))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.closeSubpath()
+            }
+            .fill(Color.purple)
         }
-        .stroke(Color.purple, lineWidth: 2)
     }
 }
 
