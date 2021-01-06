0d32d3658f919b72bbbbdc11905541813d93a17a
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 12:51:12 +0530

    Pie Chart using Arc

--- SwiftUI-PathsAndShapes/ContentView.swift
+++ SwiftUI-PathsAndShapes/ContentView.swift
@@ -11,25 +11,46 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Path() { path in
-                path.move(to: CGPoint(x: 20, y: 60))
-                path.addLine(to: CGPoint(x: 40, y: 60))
-                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 20))
-                path.addLine(to: CGPoint(x: 230, y: 60))
-                path.addLine(to: CGPoint(x: 230, y: 100))
-                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(0), endAngle: .degrees(290), clockwise: true)
                 path.closeSubpath()
             }
-            .stroke(Color.black, lineWidth: 5)
+            .fill(Color(.systemYellow))
+            
             Path() { path in
-                path.move(to: CGPoint(x: 20, y: 60))
-                path.addLine(to: CGPoint(x: 40, y: 60))
-                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 20))
-                path.addLine(to: CGPoint(x: 230, y: 60))
-                path.addLine(to: CGPoint(x: 230, y: 100))
-                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(290), endAngle: .degrees(190), clockwise: true)
+            }
+            .fill(Color(.systemBlue))
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(190), endAngle: .degrees(90), clockwise: true)
+            }
+            .fill(Color(.systemTeal))
+           
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(90), endAngle: .degrees(0), clockwise: true)
+            }
+            .fill(Color(.systemPurple))
+            .offset(x: 20, y: 20)
+            
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(90), endAngle: .degrees(0), clockwise: true)
                 path.closeSubpath()
             }
-            .fill(Color.purple)
+            .offset(x: 20, y: 20)
+            .stroke(Color(red: 52/255, green: 52/255, blue: 52/255), lineWidth: 4)
+            .overlay(
+                Text("25%")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .offset(x: 90, y: -100)
+            )
         }
     }
 }
