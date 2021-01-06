08e9cb7039737ca480c490dc3457208840c284a6
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 12:01:11 +0530

    Making a Rectangle with Stroke using paths

--- SwiftUI-PathsAndShapes/ContentView.swift
+++ SwiftUI-PathsAndShapes/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Path() { path in
+            path.move(to: CGPoint(x: 20, y: 20))
+            path.addLine(to: CGPoint(x: 300, y: 20))
+            path.addLine(to: CGPoint(x: 300, y: 200))
+            path.addLine(to: CGPoint(x: 20, y: 200))
+            path.closeSubpath()
+        }
+        .stroke(Color.green, lineWidth: 2)
     }
 }
 
